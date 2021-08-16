5795367b8ad94e246eafd29149e33cf111ab5dd2
gonzalo12348 <gonzaloarancibia48@gmail.com>
2021-08-16 15:54:21 -0300

    constantes modificados

--- src/constantes.js
+++ src/constantes.js
@@ -1,11 +1,12 @@
-import dotenv from 'dotenv'
-dotenv.config();
-// import REACT_APP_API from process.env
-const {
-    REACT_APP_API
-  } = process.env;
-// axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001'
-export const BASE_URL = process.env.REACT_APP_API || 'http://localhost:3001';
+// import dotenv from 'dotenv'
+// dotenv.config();
+// // import REACT_APP_API from process.env
+// const {
+//     REACT_APP_API
+//   } = process.env;
+
+// export const BASE_URL = process.env.REACT_APP_API || 'http://localhost:3001';
+export const BASE_URL = 'https://back-end-videogameapp.herokuapp.com'
 export const GET_15GAMES_URL = BASE_URL + '/videogames';
 export const GET_GAMES_FOUNDED_URL = GET_15GAMES_URL+ '?name=';
 export const GET_GAME_DETAILS_URL = BASE_URL + '/videogame/'
